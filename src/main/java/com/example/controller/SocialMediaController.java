d76f85f2be72f861242dd3c212d53fd534e20677
Elan Locke <elanlocke@gmail.com>
2023-12-18 18:50:26 +0000

    Small progress

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,13 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.entity.*;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +15,58 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    MessageService messageService;
 
+    @PostMapping("/register")
+    public Account postAccount(@RequestBody Account account) {
+        return null;
+    }
+
+    @PostMapping("/login")
+    public Account Login(@RequestBody Account account) {
+        Account foundAccount = accountService.login(account.getAccount_id(), account.getPassword());
+        if (foundAccount != null)
+        {
+            return foundAccount;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @PostMapping("/messages")
+    public Message postMessage(@RequestBody Message msg) {
+        return null;
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessages() {
+        return null;
+    }
+
+    @GetMapping("/messages/{id}")
+    public Message getMessage(@PathVariable int id) {
+        return null;
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public Message deleteMessage(@PathVariable int id) {
+        return null;
+    }
+
+    @PatchMapping("/messages/{id}")
+    public Message updateMessage(@PathVariable int id, @RequestBody Message msg) {
+        return null;
+    }
+
+    @GetMapping("/accounts/{id}/messages")
+    public List<Message> getAccountMessages(@PathVariable int id) {
+        return null;
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,64 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account persistAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    /**
+     * @param id
+     * @return account related to id
+     */ 
+    public Account getAccountById(int id) {
+        Optional<Account> optAccount = accountRepository.findById(id);
+        if (optAccount.isPresent())
+        {
+            return optAccount.get();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * @param id
+     * @param password
+     * @return account related to id
+     */
+    public Account login(int id, String password) {
+        Optional<Account> optAccount = accountRepository.findById(id);
+        if (optAccount.isPresent())
+        {
+            Account account = optAccount.get();
+            if (account.getPassword() == password)
+            {
+                return account;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
